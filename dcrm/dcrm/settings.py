667c557cd6ba4df5761fa46ceef300233ffc4184
Vignesh Mohan <125804139+VigneshvijayMohan@users.noreply.github.com>
2025-03-15 23:21:53 +0530

    Update settings.py
    Changed mysql password

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "elderco",
         "USER":"vignesh",
-        "PASSWORD":"kraken12@",
+        "PASSWORD":"password,
         "HOST":"localhost",
         "PORT":"3306",
     }
